d2d622ec8e032dcbf16334227d76d9337fa66b21
keiwv <brayan.perez56@uabc.edu.mx>
2024-04-01 19:12:11 -0700

    Added particles position and movement

--- include/particle.h
+++ include/particle.h
@@ -3,9 +3,21 @@
 
 using namespace std;
 
-#define MAX_PARTICLES 120
-#define PARTICLE_SPEED 1.0f
+#define MAX_PARTICLES 200
+#define PARTICLE_SPEED_X -200.0f
+#define PARTICLE_SPEED_Y 0.0f
+#define PARTICLE_SPEED_Z 0.0f
 
+#define PARTICLE_SIZE 1.0f
+#define PARTICLE_MAX_SIZE 15.0f
+
+// CHANGE COLOR OF THE PARTICLES HERE.
+#define BLUEGRAY \
+    CLITERAL(Color) { 116, 160, 195, 255 }
+#define BLUEGRAYDARK \
+    CLITERAL(Color) { 39, 42, 56, 255 }
+
+Color changeColorParticle();
 class Particle
 {
 public:
@@ -24,99 +36,122 @@ public:
 
 void Particle::updateParticle()
 {
-    this->color = (Color){GetRandomValue(0, 255), GetRandomValue(0, 255), GetRandomValue(0, 255), 255};
     this->alpha = 1.0f;
-    this->size = (float)GetRandomValue(1, 10) / 15.0f;
+    this->size = PARTICLE_SIZE + (float)GetRandomValue(1, PARTICLE_MAX_SIZE) / 15.0f;
     this->active = false;
 
     do
     {
-        this->position.x = (float)GetRandomValue(-50, 50);
-        this->position.y = (float)GetRandomValue(-50, 50);
-        this->position.z = (float)GetRandomValue(-50, 50);
-    } while (this->position.x >= -10 && this->position.x <= 10 &&
-             this->position.y >= -10 && this->position.y <= 10 &&
-             this->position.z >= -10 && this->position.z <= 10);
+        this->position.x = (float)GetRandomValue(-100, 500);
+        this->position.y = (float)GetRandomValue(-100, 100);
+        this->position.z = (float)GetRandomValue(-100, 100);
+    } while (this->position.x >= -50 && this->position.x <= 50 &&
+             this->position.y >= -20 && this->position.y <= 20 &&
+             this->position.z >= -20 && this->position.z <= 20);
 
     this->lifeSpan = (float)GetRandomValue(20, 30);
-    this->particleSpeed = (Vector3){(float)GetRandomValue(-1, 1), (float)GetRandomValue(-1, 1), (float)GetRandomValue(-1, 1)};
+    this->particleSpeed = (Vector3){PARTICLE_SPEED_X, PARTICLE_SPEED_Y, PARTICLE_SPEED_Z};
     this->growing = GetRandomValue(0, 1);
+
+    this->color = changeColorParticle();
 }
 // --------------- PROTOTYPE ----------------------------
 Particle getNewParticle();
-int initializeParticles(Particle particleses[]);
-void drawParticles(Particle particlses[], int particleCount);
-void UpdateParticlesPosition(Particle Particles[], int particleCount, float frameTime);
+int initializeParticles(Particle particles[]);
+void drawParticles(Particle particles[], int particleCount);
+void UpdateParticlesPosition(Particle particles[], int particleCount, float frameTime);
 
 // --------------- FUNCTIONS DEVELOPEMENT ----------------------------
 Particle getNewParticle()
 {
     Particle newParticle;
-    newParticle.color = (Color){GetRandomValue(0, 255), GetRandomValue(0, 255), GetRandomValue(0, 255), 255};
     newParticle.alpha = 1.0f;
-    newParticle.size = (float)GetRandomValue(1, 10) / 15.0f;
+    newParticle.size = 0.0f;
     newParticle.active = false;
     do
     {
-        newParticle.position.x = (float)GetRandomValue(-50, 50);
-        newParticle.position.y = (float)GetRandomValue(-50, 50);
-        newParticle.position.z = (float)GetRandomValue(-50, 50);
-    } while (newParticle.position.x >= -10 && newParticle.position.x <= 10 &&
-             newParticle.position.y >= -10 && newParticle.position.y <= 10 &&
-             newParticle.position.z >= -10 && newParticle.position.z <= 10);
+        newParticle.position.x = (float)GetRandomValue(-100, 300);
+        newParticle.position.y = (float)GetRandomValue(-100, 100);
+        newParticle.position.z = (float)GetRandomValue(-100, 100);
+    } while (newParticle.position.x >= -50 && newParticle.position.x <= 50 &&
+             newParticle.position.y >= -20 && newParticle.position.y <= 20 &&
+             newParticle.position.z >= -20 && newParticle.position.z <= 20);
 
     newParticle.lifeSpan = (float)GetRandomValue(20, 30);
-    newParticle.particleSpeed = (Vector3){(float)GetRandomValue(-1, 1), (float)GetRandomValue(-1, 1), (float)GetRandomValue(-1, 1)};
+    newParticle.particleSpeed = (Vector3){PARTICLE_SPEED_X, PARTICLE_SPEED_Y, PARTICLE_SPEED_Z};
     newParticle.growing = GetRandomValue(0, 1);
-    ;
+    newParticle.color = changeColorParticle();
+
     return newParticle;
 }
 
-int initializeParticles(Particle particleses[])
+int initializeParticles(Particle particles[])
 {
     int particleCount = 0;
     for (int i = 0; i < MAX_PARTICLES; i++)
     {
-        particleses[i] = getNewParticle();
+        particles[i] = getNewParticle();
         particleCount++;
     }
     return particleCount;
 }
 
-void drawParticles(Particle particlses[], int particleCount)
+void drawParticles(Particle particles[], int particleCount)
 {
     for (int i = 0; i < particleCount; i++)
     {
-        DrawSphereEx(particlses[i].position, particlses[i].size, 10, 10, particlses[i].color);
+        DrawSphereEx(particles[i].position, particles[i].size, 10, 10, particles[i].color);
     }
 }
 
-void UpdateParticlesPosition(Particle Particles[], int particleCount, float frameTime)
+void UpdateParticlesPosition(Particle particles[], int particleCount, float frameTime)
 {
 
     for (int i = 0; i < particleCount; i++)
     {
 
-        Particles[i].position.x += Particles[i].particleSpeed.x * frameTime;
-        Particles[i].position.y += Particles[i].particleSpeed.y * frameTime;
-        Particles[i].position.z += Particles[i].particleSpeed.z * frameTime;
+        particles[i].position.x += particles[i].particleSpeed.x * frameTime;
+        particles[i].position.y += particles[i].particleSpeed.y * frameTime;
+        particles[i].position.z += particles[i].particleSpeed.z * frameTime;
 
-        if (Particles[i].growing)
+        if (particles[i].position.x <= 200)
         {
-            Particles[i].size = Particles[i].size + frameTime / 3;
+            if (particles[i].position.y <= 30 && particles[i].position.y >= -30)
+            {
+                if (particles[i].position.z <= 30 && particles[i].position.z >= -30)
+                {
+                    particles[i].size = -1.0f;
+                    particles[i].growing = false;
+                }
+            }
+        }
+
+        if (particles[i].growing)
+        {
+            particles[i].size = particles[i].size + frameTime;
         }
         else
         {
-            Particles[i].size = Particles[i].size - frameTime / 3;
+            particles[i].size = particles[i].size - frameTime;
         }
 
-        if (Particles[i].growing && Particles[i].size >= 1.0f)
+        if (particles[i].growing && particles[i].size >= 1.0f)
         {
-            Particles[i].growing = false;
+            particles[i].growing = false;
         }
-        else if (!Particles[i].growing && Particles[i].size <= 0.0f)
+        else if (!particles[i].growing && particles[i].size <= 0.0f)
         {
-            Particles[i].updateParticle();
+            particles[i].updateParticle();
         }
     }
+}
+
+Color changeColorParticle()
+{
+    if (GetRandomValue(0, 1))
+    {
+        return BLUEGRAY;
+    }
+
+    return BLUEGRAYDARK;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,56 @@
 #include "../include/raylib.h"
 #include "../include/rcamera.h"
 #include "../include/raymath.h"
-#include "../include/walls.h"
+// #include "../include/walls.h"
 #include "../include/particle.h"
 #include <iostream>
 
 using namespace std;
 
-#define CAMERA_MOVE_SPEED 0.09f
-#define CAMERA_ROTATION_SPEED 0.03f
-#define CAMERA_PAN_SPEED 0.2f
-
 // Camera mouse movement sensitivity
 #define CAMERA_MOUSE_MOVE_SENSITIVITY 0.001f // TODO: it should be independant of framerate
 
-// Camera orbital speed in CAMERA_ORBITAL mode
-#define CAMERA_ORBITAL_SPEED 0.5f // Radians per second
-
-void UpdateCamera2(Camera *camera, int mode);
-void drawWalls(Wall walls);
+void UpdateCamera(Camera *camera);
+// void drawWalls(Wall walls);
 
 int main(void)
 {
 
-    const int screenWidth = GetScreenWidth();
-    const int screenHeight = GetScreenHeight();
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
 
     InitWindow(screenWidth, screenHeight, "Testing particles");
-    Wall walls;
+    // Wall walls;
 
     Particle particles[MAX_PARTICLES];
 
     int particleCount = initializeParticles(particles);
     float frameTime = GetFrameTime();
-    walls.wallPositionTop = {0.0f, 50.0f, 0.0f};
-    walls.wallPositionBottom = {0.0f, -50.0f, 0.0f};
 
-    walls.wallPositionInFront = {50.0f, 0.0f, 0.0f};
-    walls.wallPositionBehind = {-50.0f, 0.0f, 0.0f};
+    float deltaTime = 0.0f;
+    InitAudioDevice();
 
-    walls.wallPositionLeft = {-0.0f, 0.0f, -50.0f};
-    walls.wallPositionRight = {0.0f, 0.0f, 50.0f};
+    Music song = LoadMusicStream("song/song.wav");
+    SetMusicVolume(song, 0.0010f);
+    char timeText[20];
+
+    float progress;
+    // walls.wallPositionTop = {0.0f, 50.0f, 0.0f};
+    // walls.wallPositionBottom = {0.0f, -50.0f, 0.0f};
+
+    // walls.wallPositionInFront = {50.0f, 0.0f, 0.0f};
+    // walls.wallPositionBehind = {-50.0f, 0.0f, 0.0f};
+
+    // walls.wallPositionLeft = {-0.0f, 0.0f, -50.0f};
+    // walls.wallPositionRight = {0.0f, 0.0f, 50.0f};
 
     Camera3D camera = {0};
-    camera.position = (Vector3){0.0f, 0.0f, 0.0f};
-    camera.target = (Vector3){1.0f, 0.0f, 0.0f};
+    camera.position = (Vector3){0.0f, 3.0f, 0.0f};
+    camera.target = (Vector3){3.0f, 3.0f, 0.0f};
     camera.up = (Vector3){0.0f, 1.0f, 0.0f};
-    camera.fovy = 90.0f;
+    camera.fovy = 120.0f;
+
+    PlayMusicStream(song);
 
     DisableCursor();
 
@@ -55,29 +59,43 @@ int main(void)
     while (!WindowShouldClose())
     {
         frameTime = GetFrameTime();
-        UpdateCamera2(&camera, CAMERA_FREE);
+        deltaTime += frameTime;
+        UpdateCamera(&camera, CAMERA_FREE);
+        UpdateMusicStream(song);
+        progress = GetMusicTimePlayed(song);
         BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
+        ClearBackground(LOWDARK);
         BeginMode3D(camera);
 
-        drawParticles(particles, particleCount);
-        UpdateParticlesPosition(particles, particleCount, frameTime);
+        if (deltaTime > 0.5f)
+        {
+            drawParticles(particles, particleCount);
+            UpdateParticlesPosition(particles, particleCount, frameTime);
+        }
 
-        drawWalls(walls);
-        DrawCubeWires({0.0f, 0.0f, 0.0f}, 2.0f, 2.0f, 2.0f, WHITE);
-        DrawGrid(10, 1.0f);
+        // printf("X: %f, Y: %f, Z: %f\n", camera.target.x, camera.target.y, camera.target.z);
+
+        // drawWalls(walls);
+        DrawCubeWires({0.0f, 3.0f, 0.0f}, 5.0f, 5.0f, 5.0f, WHITE);
+        // DrawGrid(100, 5.0f);
+
         EndMode3D();
+
+        DrawRectangle(50, 50, 10 * progress, 20, RED);
+        sprintf(timeText, "TIME: %.2f s", progress);
+        DrawText(timeText, 50, 80, 20, WHITE);
+
         EndDrawing();
     }
-
+    StopMusicStream(song);
+    UnloadMusicStream(song);
+    CloseAudioDevice();
     CloseWindow();
 
     return 0;
 }
 
-void UpdateCamera2(Camera *camera, int mode)
+void UpdateCamera(Camera *camera)
 {
     Vector2 mousePositionDelta = GetMouseDelta();
 
@@ -85,12 +103,32 @@ void UpdateCamera2(Camera *camera, int mode)
     CameraPitch(camera, -mousePositionDelta.y * CAMERA_MOUSE_MOVE_SENSITIVITY, true, false, false);
 }
 
-void drawWalls(Wall walls)
+bool detectarBajos(float *audioData, int numSamples)
 {
-    DrawCube(walls.wallPositionTop, 100.0f, 0.0f, 100.0f, LOWDARK);
-    DrawCube(walls.wallPositionRight, 100.0f, 100.0f, 1.0f, LOWDARK);
-    DrawCube(walls.wallPositionLeft, 100.0f, 100.0f, 1.0f, LOWDARK);
-    DrawCube(walls.wallPositionInFront, 1.0f, 100.0f, 100.0f, LOWDARK);
-    DrawCube(walls.wallPositionBehind, 0.0f, 100.0f, 100.0f, LOWDARK);
-    DrawCube(walls.wallPositionBottom, 100.0f, 0.0f, 100.0f, LOWDARK);
+    float promedio = 0.0f;
+    float umbral = 0.1f;
+    for (int i = 0; i < numSamples; i++)
+    {
+        promedio += fabs(audioData[i]);
+    }
+    promedio /= numSamples;
+
+    if (promedio > umbral)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
+
+// void drawWalls(Wall walls)
+// {
+//     DrawCube(walls.wallPositionTop, 100.0f, 0.0f, 100.0f, LOWDARK);
+//     DrawCube(walls.wallPositionRight, 100.0f, 100.0f, 1.0f, LOWDARK);
+//     DrawCube(walls.wallPositionLeft, 100.0f, 100.0f, 1.0f, LOWDARK);
+//     DrawCube(walls.wallPositionInFront, 1.0f, 100.0f, 100.0f, LOWDARK);
+//     DrawCube(walls.wallPositionBehind, 0.0f, 100.0f, 100.0f, LOWDARK);
+//     DrawCube(walls.wallPositionBottom, 100.0f, 0.0f, 100.0f, LOWDARK);
+// }
